0f94878ef59a10f2dc406d825463bbc3f5fc65ea
MG SOKO <60690684+mgsohg@users.noreply.github.com>
2023-10-21 18:31:47 -0400

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -4,54 +4,71 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import torch
 import torch.nn as nn
-
-#og
-train_x1 = #load the first representations
-train_x2 = #load the second representations
-train_x3 = #load the third representations
-train_y = #Load the label
-
-'''You can use any tran_y between first, second, and third network since we extracted features without shuffling.'''
-
-# Flatten && Concat for training set
-train_x1 = train_x1.flatten()
-train_x2 = train_x2.flatten()
-
-mid_train_ = np.concatenate([train_x1,train_x2])
-mid_trainx_ = np.reshape(mid_train_,(N,1024)) ## (A,B) A: total number of N, B: Volume of feature *2
-
-Final_train = np.concatenate([train_x3,mid_train_])
-Final_train = np.reshape(Final_train,(N,1536)) ## (A,B) A: total number of N, B: Volume of feature *3
-
-Train_x, Train_y = shuffle(Final_train ,train_y) #Shuffling
-
-
-# Flatten && Concat for testing set
-
-test_x1 = #load the first representations
-test_x2 = #load the second representations
-test_x3 = #load the third representations
-test_y = #load the  label
-
-test_x1= test_x1.flatten()
-test_x2 = test_x2.flatten()
-
-mid_test = np.concatenate([test_x1,test_x2])
-mid_testx = np.reshape(mid_test,(N,1024))
-
-mid_testx = mid_testx.flatten()
-test_x3 = test_x3.flatten()
-Final_test = np.concatenate([mid_testx,test_x3])
-Final_test = np.reshape(Final_test,(N,1536))
-
-Test_x, Test_y = Final_test, test_y
-
-
-##Kmeans
-
+from sklearn.utils import shuffle
 from sklearn.cluster import KMeans
 from sklearn.metrics import confusion_matrix
+import argparse
 
-kmeans = KMeans(n_clusters=K) #Optimal K
-kmeans.fit(Train_x)
-y_pred = kmeans.predict(Test_x)
+def load_representations(path):
+    # Load representations from the given path
+    # Implement the loading logic here
+    pass
+
+def main(train_x1_path, train_x2_path, train_x3_path, train_y_path, test_x1_path, test_x2_path, test_x3_path, test_y_path, k):
+    # Load training data
+    train_x1 = load_representations(train_x1_path)
+    train_x2 = load_representations(train_x2_path)
+    train_x3 = load_representations(train_x3_path)
+    train_y = load_labels(train_y_path)
+
+    # Flatten and concatenate for the training set
+    train_x1 = train_x1.flatten()
+    train_x2 = train_x2.flatten()
+
+    mid_train_ = np.concatenate([train_x1, train_x2])
+    mid_trainx_ = np.reshape(mid_train_, (N, 1024))
+
+    Final_train = np.concatenate([train_x3, mid_train_])
+    Final_train = np.reshape(Final_train, (N, 1536))
+
+    Train_x, Train_y = shuffle(Final_train, train_y)
+
+    # Load testing data
+    test_x1 = load_representations(test_x1_path)
+    test_x2 = load_representations(test_x2_path)
+    test_x3 = load_representations(test_x3_path)
+    test_y = load_labels(test_y_path)
+
+    test_x1 = test_x1.flatten()
+    test_x2 = test_x2.flatten()
+
+    mid_test = np.concatenate([test_x1, test_x2])
+    mid_testx = np.reshape(mid_test, (N, 1024))
+
+    mid_testx = mid_testx.flatten()
+    test_x3 = test_x3.flatten()
+
+    Final_test = np.concatenate([mid_testx, test_x3])
+    Final_test = np.reshape(Final_test, (N, 1536))
+
+    Test_x, Test_y = Final_test, test_y
+
+    # Kmeans
+    kmeans = KMeans(n_clusters=k)  # Specify the optimal value of K
+    kmeans.fit(Train_x)
+    y_pred = kmeans.predict(Test_x)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="K-means clustering with feature representations.")
+    parser.add_argument('--train_x1', type=str, help='Path to first training representations', required=True)
+    parser.add_argument('--train_x2', type=str, help='Path to second training representations', required=True)
+    parser.add_argument('--train_x3', type=str, help='Path to third training representations', required=True)
+    parser.add_argument('--train_y', type=str, help='Path to training labels', required=True)
+    parser.add_argument('--test_x1', type=str, help='Path to first test representations', required=True)
+    parser.add_argument('--test_x2', type=str, help='Path to second test representations', required=True)
+    parser.add_argument('--test_x3', type=str, help='Path to third test representations', required=True)
+    parser.add_argument('--test_y', type=str, help='Path to test labels', required=True)
+    parser.add_argument('--k', type=int, help='Number of clusters (K)', required=True)
+
+    args = parser.parse_args()
+    main(args.train_x1, args.train_x2, args.train_x3, args.train_y, args.test_x1, args.test_x2, args.test_x3, args.test_y, args.k)

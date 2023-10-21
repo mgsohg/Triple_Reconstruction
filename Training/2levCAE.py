d4299dfce6a423cf42664498bdb0606fb0f9c859
MG SOKO <60690684+mgsohg@users.noreply.github.com>
2023-10-21 18:20:00 -0400

    Update 2levCAE.py

--- Training/2levCAE.py
+++ Training/2levCAE.py
@@ -1,73 +1,102 @@
-from __future__ import division, print_function
-from Backbone import *
-import torchvision.datasets as dsets
 import torch
 import torch.nn as nn
-from torchvision.utils import save_image
-from torchvision import models, transforms
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from Backbone import ConvAutoencoder
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
 
-# original data
-transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5], std=[0.5]),
-])
+def Sec_REC():
+    # Set random seed for reproducibility if needed
+    torch.manual_seed(42)
 
-Train = dsets.ImageFolder('...', transform) #put your training data path
-Test = dsets.ImageFolder('...', transform) #put your testing data path
+    # Define data transformations
+    transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5]),
+    ])
 
+    # Define the paths for training and testing data
+    train_data_path = '...'  # Replace with your training data path
+    test_data_path = '...'   # Replace with your testing data path
 
-train_loader = torch.utils.data.DataLoader(dataset=Train, batch_size=64, shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=Test, batch_size=64, shuffle=False)
+    # Create datasets
+    train_dataset = dsets.ImageFolder(train_data_path, transform=transform)
+    test_dataset = dsets.ImageFolder(test_data_path, transform=transform)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Create data loaders
+    batch_size = 64
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
-'''   
-model: 2levCAE
-load = Pretrained model
-'''
+    # Check for GPU availability
+    device = torch.device("cuda" if torch.cuda.is available() else "cpu")
 
-model = ConvAutoencoder() 
-model.to(device)
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    # Initialize the 2levCAE model
+    model = ConvAutoencoder()
+    model.to(device)
 
-load = # load pretrained model
-load.to(device)
-training_epochs = 20
-ToT_train_loss = []
-print()
-print('2lev CAE Start')
-print()
-for epoch in range(1, training_epochs + 1):
-    model.train()
-    train_loss = 0.0
-    i=0
-    for data in train_loader:
+    # Define loss function and optimizer
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-        train_image, train_label = data
-        train_image = train_image.to(device)
+    # Load a pre-trained model if needed
+    load = None  # Replace with code to load a pre-trained model
 
-        output_rec, features = load(train_image)
-        output_rec = output_rec.to(device)
+    # TensorFlow SummaryWriter for logging
+    log_dir = 'logs'  # Define your log directory
+    writer = tf.summary.create_file_writer(log_dir)
 
-        optimizer.zero_grad()
-        
-        train_output, _ = model(output_rec)
-        loss= criterion(train_output, output_rec) ##***TRAIN LOSS PART****
+    # Training parameters
+    training_epochs = 20
+    ToT_train_loss = []
 
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item() * train_image.size(0)
+    print('2lev CAE Start')
+    print()
 
-    # print avg training statistics
-    train_loss = train_loss / len(train_loader)
-    ToT_train_loss.append(train_loss)
-    print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+    for epoch in range(1, training_epochs + 1):
+        model.train()
+        train_loss = 0.0
 
+        for i, data in enumerate(train_loader):
+            train_image, train_label = data
+            train_image = train_image.to(device)
 
-save = #SAVE SECOND MODEL
+            if load is not None:
+                output_rec, features = load(train_image)
+                output_rec = output_rec.to(device)
+            else:
+                features = None  # Modify this as needed for your use case
 
+            optimizer.zero_grad()
+            train_output, _ = model(train_image)
+            loss = criterion(train_output, train_image)
+
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item() * train_image.size(0)
+
+        # Calculate average training loss for the epoch
+        train_loss = train_loss / len(train_loader)
+        ToT_train_loss.append(train_loss)
+
+        print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+
+        # Log training loss to TensorFlow
+        with writer.as_default():
+            tf.summary.scalar('Training Loss', train_loss, step=epoch)
+
+    # Save the model's computational graph to TensorFlow log directory
+    with writer.as_default():
+        graph = tf.function(model.forward)  # Convert PyTorch model to TensorFlow function
+        tf.summary.trace_on()
+        graph(train_image)
+        tf.summary.trace_export(name="model_trace", step=0, profiler_outdir=log_dir)
+
+if __name__ == "__main__":
+    main()

eed51e7757c9fdbd8b6ae7715b67b0e5ff2ed7b4
MG SOKO <60690684+mgsohg@users.noreply.github.com>
2023-10-21 18:25:49 -0400

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,94 +1,77 @@
 from __future__ import division, print_function
-from Backbone import *
-import torchvision.datasets as dsets
 import torch
 import torch.nn as nn
-from torchvision.utils import save_image
-from torchvision import models, transforms
+import torchvision.datasets as dsets
+from torchvision import transforms
 import numpy as np
-import matplotlib.pyplot as plt
+import argparse
 
-# original data
-transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5], std=[0.5]),
-])
+class ConvAutoencoder(nn.Module):
+    # Your ConvAutoencoder class definition here
 
-Loader = dsets.ImageFolder('...', transform)
-Ext_Loader = torch.utils.data.DataLoader(dataset=Loader, batch_size=1, shuffle=False)
+def feature_extraction(data_loader, load, dual, output_dir, device):
+    load.to(device)
+    dual.to(device)
 
+    print('Start Feature Extraction')
+    print()
+    for epoch in range(1, 2):
+        load.eval()
+        Feature_Y = np.empty([0, 1])
+        Feature_X = []
+        i = 0
+        for data1 in data_loader:
+            images, label = data1
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            labels_np = label.numpy()
+            Feature_Y = np.concatenate((Feature_Y, labels_np), axis=None)
 
+            # final
+            images = images.to(device)
+            output__, features__ = load(images)
+            output__ = output__.to(device)
+            output, features = dual(output__)
 
+            features = features.cpu().detach().numpy()
+            features = features.flatten()
+            Feature_X.append(features)
 
-'''Feature EXTRACTION of the first network'''
+        Feature_X = np.array(Feature_X)
+        # Save Feature_X and Feature_Y separately (This is for additional networks)
 
-load = #LOAD THE PRETRAINED model
-load.to(device)
+def main(args):
+    # Define data transformations
+    transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5]),
+    ])
 
-print('Start Feature Extraction')
-print()
-for epoch in range(1, 2):
-    dual.eval()
-    load.eval()
-    Feature_Y = np.empty([0, 1]) # For LABEL 
-    Feature_X = [] # For REPRESENTATION 
-    i=0
-    for data1 in Ext_Loader:
-        images, label = data1
+    # Create DataLoader for original data
+    Loader = dsets.ImageFolder(args.data_path, transform=transform)
+    Ext_Loader = torch.utils.data.DataLoader(dataset=Loader, batch_size=1, shuffle=False)
 
-        labels_np = label.numpy()
-        Feature_Y = np.concatenate((Feature_Y, labels_np), axis=None)
+    # Check for GPU availability
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        # General
-        images = images.to(device)
-        output, features = load(images)
+    # Load the pretrained models (replace None with actual model loading)
+    load_baseline = None
+    load_additional = None
+    dual = None
 
-        features = features.cpu().detach().numpy()
-        features = features.flatten()
-        Feature_X.append(features)
+    if args.baseline:
+        feature_extraction(Ext_Loader, load_baseline, dual, args.output_dir, device)
 
-Feature_X = np.array(Feature_X) '''THE FINAL LATENT REPRESENTATIONS OF LOADER (Ext_Loader)'''
+    if args.additional:
+        feature_extraction(Ext_Loader, load_additional, dual, args.output_dir, device)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Feature Extraction Script")
+    parser.add_argument('--data_path', type=str, help='Path to data folder', required=True)
+    parser.add_argument('--output_dir', type=str, help='Output directory for features', required=True)
+    parser.add_argument('--baseline', action='store_true', help='Perform baseline feature extraction')
+    parser.add_argument('--additional', action='store_true', help='Perform additional feature extraction')
 
-##SAVE Feature_X and Feature_Y separately, This is for the baseline network
-
-
-
-'''Feature EXTRACTION of ADDITIONAL NETWORKS'''
-
-load = #LOAD THE PRETRAINED NETWORK (1st level)
-load.to(device)
-dual = #LOAD 2nd and 3rd model separately.
-
-'''Note that,for the variable "dual", you can selectively load  a specific model either 2nd level nework or 3rd level. You have to extract both of them separately. '''
-
-
-print('Start Feature Extraction')
-print()
-for epoch in range(1, 2):
-    load.eval()
-    Feature_Y = np.empty([0, 1])
-    Feature_X = []
-    i=0
-    for data1 in Ext_Loader:
-        images, label = data1
-
-        labels_np = label.numpy()
-        Feature_Y = np.concatenate((Feature_Y, labels_np), axis=None)
-
-        #final
-        images = images.to(device)
-        output__, features__ = load(images)
-        output__ = output__.to(device)
-        output, features = dual(output__)
-
-        features = features.cpu().detach().numpy()
-        features = features.flatten()
-        Feature_X.append(features)
-Feature_X = np.array(train_x)
-
-##SAVE Feature_X and Feature_Y separately (This is for additional networks)
+    args = parser.parse_args()
+    main(args)
